33982f01a3b69f178ff1f6bdd039baf54801eb2f
Alan Yu <alan21120000@gmail.com>
2020-05-23 17:29:28 +0800

    specify the unique tag to deploy into k8s

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t alan2112000/multi-client -f ./client/Dockerfile ./client 
-docker build -t alan2112000/multi-server -f ./server/Dockerfile ./server
-dokcer build -t alan2112000/multi-worker -f ./worker/Dockerfile ./worker
-docker push alan2112000/multi-client 
-docker push alan2112000/multi-server
-docker push alan2112000/multi-worker
+docker build -t alan2112000/multi-client -t alan2112000/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t alan2112000/multi-server -t alan2112000/multi-server:$SHA -f ./server/Dockerfile ./server
+dokcer build -t alan2112000/multi-worker -t alan2112000/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push alan2112000/multi-client:latest
+docker push alan2112000/multi-server:latest
+docker push alan2112000/multi-worker:latest
+
+docker push alan2112000/multi-client:$SHA
+docker push alan2112000/multi-server:$SHA
+docker push alan2112000/multi-worker:$SHA
 
 kubelctl apply -f k8s 
-kubectl set image deployments/server-deployment server=alan2112000/multi-server
-
+kubectl set image deployments/server-deployment server=alan2112000/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=alan2112000/multi-worker:$SHA
+kubectl set image deployments/client-deployment server=alan2112000/multi-client:$SHA
+ 
